106c2215714c43ed3d526909d5d374effe6f6eb9
mafoloEmmanuel <mafoloemmanuel1@gmail.com>
2021-11-12 16:54:53 +0200

    done with tests

--- fruit_basket.js
+++ fruit_basket.js
@@ -1,67 +1,56 @@
-const {Pool} = require('pg');
-
-
-let useSSL =false;
- let local = process.env.LOCAL || false;
- if(process.env.DATABASE_URL && !local){
-     useSSL = { rejectUnauthorized: false };
- }
- //choosing a db connection 
- const connectionString = process.env.DATABASE_URL || 'postgresql://coder:201735469@localhost:5432/coderdb'
- //connect with a connection pool
-     const pool = new Pool({
-         connectionString: connectionString,
-       ssl:  useSSL
-     });
-pool.on('connect', ()=>{
-    console.log('connection has started')
-});
 
 module.exports = function FruitBasket(pool) {
-  
-let setFruitBasket= async(type,price)=>{
-    var quantity = 1
-   var result = await pool.query('insert into fruit_basket (fruit_name,price,quantity) values ($1,$2,$3)', [type,price,quantity]);
-   return result.rows
-}
 
-//Find all the fruit baskets for a given fruit type. 
-let getFruitBasket = async(type)=>{
+    // insert fruit type, price and number of fruits
+    let setFruitBasket = async (type, price, qty) => {
+        var checkBasket = await pool.query('select fruit_name from fruit_basket where fruit_name=$1',[type]);
+        //console.log(checkBasket.rows);
+        if(checkBasket.rows.length <1){
+            var result = await pool.query('insert into fruit_basket (fruit_name,price,quantity) values ($1,$2,$3)', [type, price, qty]);
+         
+            return result.rows
+        } else {
+            return await updateNumberOfFruits(type,qty)
+        }
+    }
 
-   var result= await pool.query('select * from fruit_basket where fruit_name=$1',[type]);
-  console.log(result.rows)
-   return result.rows
-}
+    //Find all the fruit baskets for a given fruit type. 
+    let getFruitBasket = async (type) => {
 
-//Update the number of fruits in a given basket.
+        var result = await pool.query('select fruit_name,price,quantity  from fruit_basket where fruit_name=$1', [type]);
+       // console.log(result.rows)
+        return result.rows
+    }
 
-let updateNumberOfFruits = async(type)=>{
-    var sql =' update fruit_basket set quantity=quantity + 1 where fruit_name=$1'
-    var result = await pool.query(sql,[type])
-    return result.rows
-}
+    //Update the number of fruits in a given basket.
 
-//  Show the total price for a given fruit basket.
-let basketTotalPrice =async(type)=>{
-    var sql = 'select fruit_name, sum(price) as fruit_total from fruit_basket where fruit_name=$1 group by fruit_name';
-var result = await pool.query(sql,[type]);
-console.log(result.rows)
-return result.rows
-}
-// count how many fruits in each basket
-let countFruits = async(type)=>{
-var result = await pool.query('select fruit_name,count(*) as basket_count from fruit_basket where fruit_name=$1 group by fruit_name',[type])
+    let updateNumberOfFruits = async (type, qty) => {
+        var sql = ' update fruit_basket set quantity=quantity + $2 where fruit_name=$1'
+        var result = await pool.query(sql, [type, qty])
+        return result.rows
+    }
 
-return result.rows
+    //  Show the total price for a given fruit basket.
+    let basketTotalPrice = async (type) => {
+        var sql = 'select fruit_name, sum(price * quantity ) as fruit_total from fruit_basket where fruit_name=$1 group by fruit_name';
+        var result = await pool.query(sql, [type]);
+     //   console.log(result.rows)
+        return result.rows
+    }
+    // count how many fruits in each basket
+    /*
+    let countFruits = async (type) => {
+        var result = await pool.query('select fruit_name,count(*) as basket_count from fruit_basket where fruit_name=$1 group by fruit_name', [type])
 
-}
+        return result.rows
 
-return{
-    setFruitBasket,
-    getFruitBasket,
-    countFruits,
-    updateNumberOfFruits,
-    basketTotalPrice
-}
+    }*/
+
+    return {
+        setFruitBasket,
+        getFruitBasket,
+        updateNumberOfFruits,
+        basketTotalPrice
+    }
 
 }
--- test/basket.test.js
+++ test/basket.test.js
@@ -14,72 +14,135 @@ const pool = new Pool({
     ssl: useSSL
 });
 pool.on('connect', () => {
-    console.log('connection has started')
+  //  console.log('connection has started')
 });
 const mocha = require('mocha');
 const assert = require('assert');
+const { describe, it } = require('mocha');
 
 describe('Fruit basket ', () => {
 
     beforeEach(async () => {
-        console.log('*************');
+       // console.log('*************');
         await pool.query('delete from fruit_basket');
     })
     let fruity = FruitBasket(pool)
-
-    it('show fruits', async () => {
-        await fruity.setFruitBasket('Apple', 2.50);
-        await fruity.setFruitBasket('Apple', 2.50);
-        await fruity.setFruitBasket('Banana', 1.50);
-        await fruity.setFruitBasket('Mango', 3.50);
-
-
-        assert.equal(1, await fruity.getFruitBasket('Apple').length)
-    })
-    it('Show that there are two apples in an apples basket', async () => {
-        await fruity.setFruitBasket('Apple',2.50);
-        await fruity.setFruitBasket('Apple',2.50)
+describe('Setting fruit baskets', ()=>{
+    it('Should set basket of Apples ',async()=>{
+        await fruity.setFruitBasket('Apples',2.50,7);
+     
+        assert.deepEqual([
+         {
+           fruit_name: 'Apples',
+           price: '2.50',
+           quantity: 7
+         }
+       ], await fruity.getFruitBasket('Apples'));
+     });
+     
+     
+     it('Should set basket of Bananas ',async()=>{
+         await fruity.setFruitBasket('Bananas',1.50,6);
       
-
-        assert.deepEqual([
-            {
-              basket_count: '2',
-              fruit_name: 'Apple'
-            }
-          ]
-          , await fruity.countFruits('Apple'));
-
-    });
-    it('Show that there are three bananas in a banana basket', async () => {
-        await fruity.setFruitBasket('Banana',1.50);
-        await fruity.setFruitBasket('Banana',1.50);
-        await fruity.setFruitBasket('Banana',1.50);
-
+         assert.deepEqual([
+          {
+            fruit_name: 'Bananas',
+            price: '1.50',
+            quantity: 6
+          }
+        ], await fruity.getFruitBasket('Bananas'));
+      });
       
+     it('Should set basket of Oranges ',async()=>{
+         await fruity.setFruitBasket('Oranges',3.00,10);
+      
+         assert.deepEqual([
+          {
+            fruit_name: 'Oranges',
+            price: '3.00',
+            quantity: 10
+          }
+        ], await fruity.getFruitBasket('Oranges'));
+      });
+     
+});
+describe('Update the number of fruits in a given basket',()=>{
+    it('Update the Apples basket', async()=>{
+        await fruity.setFruitBasket('Apples',2.50,7);
 
-        assert.deepEqual([
-            {
-              basket_count: '3',
-              fruit_name: 'Banana'
-            }
-          ]
-          , await fruity.countFruits('Banana'));
-
+        await fruity.updateNumberOfFruits('Apples', 7);
+        assert.deepEqual(
+            [
+              {
+                fruit_name: 'Apples',
+                price: '2.50',
+                quantity: 14
+              }
+            ], await fruity.getFruitBasket('Apples'))
     });
-    it('Get the total price for a given fruit basket', async() => {
-        await fruity.setFruitBasket('Apple', 2.50);
-        await fruity.setFruitBasket('Apple', 2.50);
-        await fruity.setFruitBasket('Apple',2.50);
-        await fruity.setFruitBasket('Apple',2.50);
 
-        assert.deepEqual([
+it('Update the Bananas basket', async()=>{
+    await fruity.setFruitBasket('Bananas',1.50,6);
+
+    await fruity.updateNumberOfFruits('Bananas',10);
+    assert.deepEqual(
+        [
+          {
+            fruit_name: 'Bananas',
+            price: '1.50',
+            quantity: 16
+          }
+        ], await fruity.getFruitBasket('Bananas'))
+});
+it('Update the basket of Oranges ',async()=>{
+    await fruity.setFruitBasket('Oranges',3.00,10);
+    await fruity.updateNumberOfFruits('Oranges',15);
+    
+ 
+    assert.deepEqual([
+     {
+       fruit_name: 'Oranges',
+       price: '3.00',
+       quantity: 25
+     }
+   ], await fruity.getFruitBasket('Oranges'));
+ });
+});
+
+describe('Show the total price for a given fruit basket',()=>{
+    it('Should show a total for Apples basket', async()=>{
+        await fruity.setFruitBasket('Apples',2.50,7);
+        assert.deepEqual( [
             {
-              fruit_name: 'Apple',
-              fruit_total: '10.00'
+              fruit_name: 'Apples',
+              fruit_total: 17.50
             }
-          ], await fruity.basketTotalPrice('Apple') );
+          ], await fruity.basketTotalPrice('Apples'));
+        
     });
 
+it('Should show a total for Bananas basket', async()=>{
+    await fruity.setFruitBasket('Bananas',1.50,10);
+    assert.deepEqual( [
+        {
+          fruit_name: 'Bananas',
+          fruit_total: 15.00
+        }
+      ], await fruity.basketTotalPrice('Bananas'));
+    
+});
+it('Should show a total for Oranges basket', async()=>{
+    await fruity.setFruitBasket('Oranges',3.00,9);
+    assert.deepEqual( [
+        {
+          fruit_name: 'Oranges',
+          fruit_total: 27
+        }
+      ], await fruity.basketTotalPrice('Oranges'));
+    
+});
+
+});
 
     after(() => {
         pool.end()
